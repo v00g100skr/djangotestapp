89771be13f7ab4f39d1d98904c1785aeb3211b66
utmcrow <utmcrow@gmail.com>
2016-08-26 00:51:27 +0300

    base index and character pages

--- starwars/views.py
+++ starwars/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
 
+from .models import Characters
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    characters_list = Characters.objects.order_by('name')
+    context = {
+        'characters_list': characters_list,
+    }
+    return render(request, 'starwars/index.html', context)
+
+
+def character(request, character_id):
+    try:
+        character = Characters.objects.get(pk=character_id)
+        context = {
+            'character': character,
+        }
+    except Characters.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'starwars/details.html', context)
